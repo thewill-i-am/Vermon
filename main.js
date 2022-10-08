1d40454e36c1f6e872e507b563acb87ce06ac578
Isabella Aguilar <ia514648@gmail.com>
2022-10-08 00:14:30 -0600

    creacion empresa y formulario empresa

--- main.js
+++ main.js
@@ -9,34 +9,33 @@ const corsOptions = {
 }
 const app = express();
 app.use(cors(corsOptions));
+
 const db = require('./database/db');
 
 (async () => {
     await db.sequelize.sync()
 })();
 
-db.models.Usuario
 
-// var bodyParser = require("body-parser");
-// const path = require('path');
+var bodyParser = require("body-parser");
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "100mb" }));
 
-// const port = 3001;
+const port = 3001;
 
-// //static files
-// app.use(express.static("layers"));
-// app.use("/layers", express.static(__dirname + "layers/background"));
-// app.use("/layers", express.static(__dirname + "layers/Bottom Lid"));
-// app.use("/layers", express.static(__dirname + "layers/Eye color"));
-// app.use("/layers", express.static(__dirname + "layers/Eyeball"));
-// app.use("/layers", express.static(__dirname + "layers/Goo"));
-// app.use("/layers", express.static(__dirname + "layers/Iris"));
-// app.use("/layers", express.static(__dirname + "layers/Shine"));
-// app.use("/layers", express.static(__dirname + "layers/Top lid"));
+//static files
+app.use(express.static("layers"));
+app.use("/layers", express.static(__dirname + "layers/background"));
+app.use("/layers", express.static(__dirname + "layers/Bottom Lid"));
+app.use("/layers", express.static(__dirname + "layers/Eye color"));
+app.use("/layers", express.static(__dirname + "layers/Eyeball"));
+app.use("/layers", express.static(__dirname + "layers/Goo"));
+app.use("/layers", express.static(__dirname + "layers/Iris"));
+app.use("/layers", express.static(__dirname + "layers/Shine"));
+app.use("/layers", express.static(__dirname + "layers/Top lid"));
 
-// app.use("/api", require("./routes/ruta"));
+app.use("/api", require("./routes/ruta"));
 
-// app.listen(port, () => console.info(`Listening on port ${port}`));
+app.listen(port, () => console.info(`Listening on port ${port}`));
 
--- routes/ruta.js
+++ routes/ruta.js
@@ -1,58 +1,79 @@
 const express = require("express");
 const router = express.Router();
 const startCreating = require("../src/logic/nft");
-const { Usuario, FormularioSolicitud, Evidencia, Empresa, FormularioUsuarioEmpresa} = require("../database/db");
+const {
+    models: {
+        Usuario,
+        Empresa,
+        SolicitudEmpresa,
+        SolicitudNFT,
+        TipoNFT,
+        TipoEmpresa,
+    },
+} = require("../database/db");
 
-router.get("/generador", async(req, res) => {
-    console.log("llego aqui")
+router.get("/generador", async (req, res) => {
     var x = await startCreating();
     res.send({ imagenes: x.dat, jsonContract: x.textPadre });
 });
 
-router.post("/crearEmpresa", async(req, res, next) => {
-    const empresa = new Empresa();
-    //TODO: Pasar parametros
-    // await empresa.insert();
-    res.sendStatus(200);
+router.post("/crearSolicitudEmpresa", async (req, res, next) => {
+    try
+    {
+        var solicitudEmpresa = await SolicitudEmpresa.create({
+            nombre: req.body.nombre,
+            longitud: req.body.longitud,
+            latitud: req.body.latitud,
+            numeroTelefono: req.body.numeroTelefono,
+            fechaSolicitud: req.body.fechaSolicitud,
+            estaAprobado: 0,
+            idTipoEmpresa: req.body.idTipoEmpresa,
+        });
+        res.status(200).json({'success' : true, 'empresa': solicitudEmpresa})
+    } catch (err)
+    {
+        res.status(500).send(err);
+    }
 
+    next();
 });
 
-router.post("/crearFormularioSolicitud", async(req, res, next) => {
-    const formularioSolicitud = new FormularioSolicitud();
-    //TODO: Pasar parametros
+router.post("/crearEmpresa/:id", async (req, res, next) => {
+    try { 
+        console.log(req.params.id)
+        var sol = await SolicitudEmpresa.findOne({ where: { id: req.params.id } });
+        sol.set({
+            estaAprobado : true
+        })
+        await sol.save()
+        var empresa = await Empresa.create({
+            idSolicitudEmpresa : sol.id
+        })
+        res.status(200).json({'success' : true, 'sol': empresa})
+    }catch(err){
+        res.status(500).send(err);
+    }
 
-    console.log(req.body)
-    await formularioSolicitud.insert(req.body.Documento);
-    res.sendStatus(200);
+    next();
 });
 
-
-router.post("/crearEvidencias", async(req, res, next) => {
-    const evidencia = new Evidencia();
+router.post("/crearEvidencias", async (req, res, next) => {
     //TODO: Pasar parametros
-
-    console.log(req.body)
+    console.log(req.body);
     await evidencia.insert(req.body.Documento);
     res.sendStatus(200);
-    
 });
 
-router.post("/conectarUsuarioGoogle", async(req, res, next) => {
-    const usuario = new Usuario();
+router.post("/conectarUsuarioGoogle", async (req, res, next) => {
     //TODO: Pasar parametros
     // await usuario.insert();
     res.sendStatus(200);
-}); 
-
-router.post("/crearFormularioUsuarioEmpresa", async(req, res, next) => {
-    const formularioUsuarioEmpresa = new FormularioUsuarioEmpresa();
-    //TODO: Pasar parametros
-
-    console.log(req.body)
-    await formularioUsuarioEmpresa.insert(req.body.Id,req.body.Nombre);
-    res.sendStatus(200);
-   
 });
 
+router.post("/crearFormularioUsuarioEmpresa", async (req, res, next) => {
+    console.log(req.body);
+    await formularioUsuarioEmpresa.insert(req.body.Id, req.body.Nombre);
+    res.sendStatus(200);
+});
 
 module.exports = router;
